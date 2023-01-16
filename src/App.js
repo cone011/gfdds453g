5995e5afe2816b06835c6e5ffd63370b67489ab4
Juan <jjcaimen@gmail.com>
2023-01-16 20:25:55 -0300

    commit

--- src/App.js
+++ src/App.js
@@ -1,42 +1,46 @@
-import React, { Component } from 'react';
-import {
-  Switch,
-  Route,
-  Redirect,
-  Link
-} from 'react-router-dom';
+import React, { Component } from "react";
+import { Switch, Route, Redirect, Link } from "react-router-dom";
 
 export class App extends Component {
   render() {
     return (
       <div>
         <nav>
-          {/*Aca deben ir los links de navegacion*/}
+          <Route>
+            <Link to="/page1">Pagina 1</Link>
+            <Link to="/page2">Pagina 2</Link>
+          </Route>
         </nav>
-        {/* Aca tienes que agreager algo para que las rutas funcionen*/}
+        <Switch>
+          <Route path="/" exact>
+            <Redirect to="/page1" />
+          </Route>
+          <Route path="/page1">
+            <Page1 />
+          </Route>
+          <Route path="/page2">
+            <Page2 />
+          </Route>
+          <Route path="/">
+            <NotFound />
+          </Route>
+        </Switch>
       </div>
-    )
+    );
   }
 }
 
 // No tienes que hacer nada por debajo de esta linea
 //==================================================
 
-
 export const Page1 = (props) => {
-  return (
-    <h1>Pagina 1</h1>
-  )
-}
+  return <h1>Pagina 1</h1>;
+};
 
 export const Page2 = (props) => {
-  return (
-    <h1>Pagina 2</h1>
-  )
-}
+  return <h1>Pagina 2</h1>;
+};
 
 export const NotFound = (props) => {
-  return (
-    <h1>Pagina no encontrada</h1>
-  )
-}
+  return <h1>Pagina no encontrada</h1>;
+};
